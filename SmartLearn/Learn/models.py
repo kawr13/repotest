1bd40a49e1b0706e3a70adbb9f09db0d8d4be9d0
Tooliy <tuvanchap_tumen@mail.ru>
2023-10-09 21:16:18 +0700

    Start

--- SmartLearn/Learn/models.py
+++ SmartLearn/Learn/models.py
@@ -5,17 +5,16 @@ from django.db import models
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
     teacher_id = models.OneToOneField('Teacher', on_delete=models.CASCADE)
-    # requisites_id = models.OneToOneRel('Requisites', on_delete=models.CASCADE)
+    requisites_id = models.OneToOneField('Requisites', on_delete=models.CASCADE)
 
 
 class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField()
 
-#
-# class Requisites(models.Model):
-#     card_number = models.CharField(max_length=16)
-#     card_date = models.DateField()
-#     card_cvv = models.CharField(max_length=3)
-#
+
+class Requisites(models.Model):
+    card_number = models.CharField(max_length=16)
+    card_date = models.DateField()
+    card_cvv = models.CharField(max_length=3)
+
 
